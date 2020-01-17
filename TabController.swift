21046a1279aa7af1b2fde3ffcb774762e7b58c35
airy <airy@iwahashinoMacBook-Pro.local>
2020-01-17 13:37:35 +0900

    add TabBar

--- TabController.swift
+++ TabController.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import UIKit
+
+
+class TabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var viewControllers: [UIViewController] = []
+
+        let firstViewController = ViewController()
+        firstViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.mostRecent, tag: 1)
+        viewControllers.append(firstViewController)
+
+        let secondViewController = ViewController()
+        secondViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.mostViewed, tag: 2)
+        viewControllers.append(secondViewController)
+
+        let thirdViewController = ViewController()
+        thirdViewController.tabBarItem = UITabBarItem(tabBarSystemItem: UITabBarItem.SystemItem.contacts, tag: 3)
+        viewControllers.append(thirdViewController)
+
+        self.setViewControllers(viewControllers, animated: false)
+    }
+}
